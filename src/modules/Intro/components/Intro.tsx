56e27b00c3bd4f230d70dfde2dd30eecf3bd99a2
Oleh-Kril <oleh.kril.t@gmail.com>
2024-01-01 21:10:08 +0200

    add intro block (draft)

---
+++ src/modules/Intro/components/Intro.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import styles from '../styles/Intro.module.scss'
+
+export default function Intro(){
+    return (
+        <div>
+            <Parallax pages={1} style={{ top: '0', left: '0' }}>
+                <ParallaxLayer offset={0} speed={0.4}>
+                    <div className={styles.dudeContainer}>
+                        <Image alt={'dude-celebrating'}
+                               src={'/characters/dude-celebrating.webp'}
+                               width={413} height={604}
+                               className={styles.dude}/>
+                    </div>
+                </ParallaxLayer>
+            </Parallax>
+        </div>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Link from "next/link"
 import {useUser} from "@auth0/nextjs-auth0/client"
 import {useEffect} from "react"
 import {useRouter} from "next/router"
+import Intro from "../modules/Intro"
 
 export default function Home() {
     const { user, error, isLoading } = useUser()
@@ -14,8 +15,11 @@ export default function Home() {
     }, [user])
 
     return (
-      <div style={{display: "flex", gap: "2rem"}}>
-          <Link href='/api/auth/login'>Login</Link>
-      </div>
+        <div style={{height: '300vh'}}>
+            <Intro />
+            <div style={{position: "absolute", top: "55%", left: '50%', color: "#FA4441", fontWeight: "bold", fontSize: "20px"}}>
+                <Link href='/api/auth/login'>Login</Link>
+            </div>
+        </div>
     )
 }

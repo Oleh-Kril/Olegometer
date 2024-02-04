f01a585665aee468226ffb3fa509197318b52a2f
Oleh-Kril <oleh.kril.t@gmail.com>
2024-02-04 14:04:38 +0200

    add vercel plugins

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/react'
 
 export default function Document() {
     return (
@@ -12,6 +14,8 @@ export default function Document() {
                 <div id="modal-root"></div>
                 <div id="loader-root"></div>
                 <NextScript />
+                <SpeedInsights />
+                <Analytics />
             </body>
         </Html>
     )

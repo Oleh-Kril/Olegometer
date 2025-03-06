2c0aadbeb1a6a061f51f27818562578bcfa54453
Oleh-Kril <oleh.kril.t@gmail.com>
2025-03-06 19:07:38 +0200

    uninstall extra libraries from the client and adapt Dockerfile

--- src/utils/transformIdProperty.ts
+++
@@ -1,4 +0,0 @@
-export default function transformIdProperty<T extends { _id: any }>(obj: T): Omit<T, '_id'> & { id: string } {
-    const { _id, ...rest } = obj
-    return { ...rest, id: _id.toString() }
-}
